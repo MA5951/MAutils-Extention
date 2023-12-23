4dceb19dba13267ab185bbdbdc50ea51aed3b472
GalDotan <gldotan@gmail.com>
2023-12-23 22:48:19 +0200

    code completed

--- src/functions/insertCodeIntoFile.ts
+++ src/functions/insertCodeIntoFile.ts
@@ -2,7 +2,7 @@ import * as fs from 'fs';
 import * as path from 'path';
 import * as vscode from 'vscode';
 
-enum InsertLocation {
+export enum InsertLocation {
     StartOfFunction,
     EndOfFunction,
     StartOfClass,
